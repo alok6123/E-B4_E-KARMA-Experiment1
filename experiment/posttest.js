8aa06746a47901c2345a6ce285ff90f544dfe630
alok6123 <69812144+alok6123@users.noreply.github.com>
2020-09-14 15:39:13 +0530

    Update posttest.js

--- experiment/posttest.js
+++ experiment/posttest.js
@@ -90,25 +90,47 @@
 
   const myQuestions = [
     {
-      question: "This is question 1?",  ///// Write the question inside double quotes
+      question: "1. Which of the following is equal to operator?",  ///// Write the question inside double quotes
       answers: {
-        a: "Option 1",                  ///// Write the option 1 inside double quotes
-        b: "Option 2",                  ///// Write the option 2 inside double quotes
-        c: "Option 3",                  ///// Write the option 3 inside double quotes
-        d: "Option 4"                   ///// Write the option 4 inside double quotes
+        a: "=",                  ///// Write the option 1 inside double quotes
+        b: "==",                  ///// Write the option 2 inside double quotes
+        c: "=!	",                  ///// Write the option 3 inside double quotes
+        d: "=?"                   ///// Write the option 4 inside double quotes
       },
-      correctAnswer: "a"                ///// Write the correct option inside double quotes
+      correctAnswer: "b"                ///// Write the correct option inside double quotes
     },
 
     {
-      question: "This is question 2?",  ///// Write the question inside double quotes
+      question: "2. Which of the following is XOR?",  ///// Write the question inside double quotes
       answers: {
-        a: "Option 1",                  ///// Write the option 1 inside double quotes
-        b: "Option 2",                  ///// Write the option 2 inside double quotes
-        c: "Option 3",                  ///// Write the option 3 inside double quotes
-        d: "Option 4"                   ///// Write the option 4 inside double quotes
+        a: " ^ ",                  ///// Write the option 1 inside double quotes
+        b: "&&",                  ///// Write the option 2 inside double quotes
+        c: " ||",                  ///// Write the option 3 inside double quotes
+        d: "=="                   ///// Write the option 4 inside double quotes
+      },
+      correctAnswer: "a"                ///// Write the correct option inside double quotes
+    },                                  ///// To add more questions, copy the section below 
+    									                  ///// this line
+ {
+      question: "3.What is the function of an operator?",  ///// Write the question inside double quotes
+      answers: {
+        a: " Add two operand. ",                  ///// Write the option 1 inside double quotes
+        b: "Perform a mathematical and logical operator	",                  ///// Write the option 2 inside double quotes
+        c: " It perform only logical operation",                  ///// Write the option 3 inside double quotes
+        d: "None of these"                   ///// Write the option 4 inside double quotes
       },
       correctAnswer: "b"                ///// Write the correct option inside double quotes
+    },                                  ///// To add more questions, copy the section below 
+    									                  ///// this line
+ {
+      question: "4. Which file is include in C while file compiling the code",  ///// Write the question inside double quotes
+      answers: {
+        a: " conio.h ",                  ///// Write the option 1 inside double quotes
+        b: "stdio.h",                  ///// Write the option 2 inside double quotes
+        c: " iostream.h",                  ///// Write the option 3 inside double quotes
+        d: "Both a & b"                   ///// Write the option 4 inside double quotes
+      },
+      correctAnswer: "d"                ///// Write the correct option inside double quotes
     },                                  ///// To add more questions, copy the section below 
     									                  ///// this line
 
